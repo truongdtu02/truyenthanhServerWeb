a7608dcad5048e9f037fb5b5c00a47cc89b27bb6
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-06-30 16:02:36 +0700

    show ffmpeg progress

--- truyenthanhServerWeb/ServerMp3/FFmpegXabe.cs
+++ truyenthanhServerWeb/ServerMp3/FFmpegXabe.cs
@@ -45,11 +45,11 @@ namespace truyenthanhServerWeb.ServerMp3
                 .SetPreset(ConversionPreset.UltraFast)
             .AddParameter("-f mp3 udp://127.0.0.1:" + port.ToString());
             //Add log to OnProgress
-            //conversion.OnProgress += async (sender, args) =>
-            //{
-            //    //Show all output from FFmpeg to console
-            //    await Console.Out.WriteLineAsync($"[{args.Duration}/{args.TotalLength}][{args.Percent}%]");
-            //};
+            conversion.OnProgress += async (sender, args) =>
+            {
+                //Show all output from FFmpeg to console
+                await Console.Out.WriteLineAsync($"[{args.Duration}/{args.TotalLength}][{args.Percent}%]");
+            };
 
             //conversion.OnProgress += (duration, length) => { currentProgress = duration; }
 
