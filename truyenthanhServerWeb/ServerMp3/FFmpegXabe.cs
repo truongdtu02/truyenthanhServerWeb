e67f409aa00863b6b513d2947282b9da1ae2736f
truong.hd154023 <truong.hd154023@sis.hust.edu.vn>
2021-07-19 12:55:30 +0700

    update pause

--- truyenthanhServerWeb/ServerMp3/FFmpegXabe.cs
+++ truyenthanhServerWeb/ServerMp3/FFmpegXabe.cs
@@ -94,22 +94,23 @@ namespace truyenthanhServerWeb.ServerMp3
         public void PauseConversion()
         {
             //Process.Start("/bin/bash", "kill -s SIGSTOP " + processID);
-            using var process = new Process()
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "/bin/bash",
-                    //Arguments = $"kill -s SIGSTOP {processID}",
-                    Arguments = $"echo {processID}",
-                    RedirectStandardOutput = false,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                }
-            };
+            Process.Start("echo", $"{processID}");
+            //using var process = new Process()
+            //{
+            //    StartInfo = new ProcessStartInfo
+            //    {
+            //        FileName = "/bin/bash",
+            //        //Arguments = $"kill -s SIGSTOP {processID}",
+            //        Arguments = $"echo {processID}",
+            //        RedirectStandardOutput = false,
+            //        UseShellExecute = false,
+            //        CreateNoWindow = true,
+            //    }
+            //};
 
-            process.Start();
+            //process.Start();
             //string result = process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
+            //process.WaitForExit();
         }
 
         public void ResumeConversion()
